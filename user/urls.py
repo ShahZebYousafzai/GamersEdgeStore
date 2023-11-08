b37e9d896daebeaef07ba17f0c7316c2df4ba1c6
ShahZebYousafzai <zebshah7851@gmail.com>
2023-11-08 14:19:42 +0500

    Added logout

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ from user import views
 urlpatterns = [
     path("login/", views.loginView, name='login'),
     path("register/", views.registerView, name='register'),
+    path("logout/", views.logoutView, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -6,13 +6,10 @@ from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def loginView(request):
-
-    page='login'
-
     if request.user.is_authenticated:
         return redirect('home')
     
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -23,24 +20,13 @@ def loginView(request):
             return redirect('home')
         else:
             messages.error(request, 'There was an error in logging in!')
-            return redirect('home')
-
-
-        # try:
-        #     user = User.objects.get(username=username, password=password)
-        # except:
-        #     messages.error(request, 'Username does not exist')
-
-        # user = authenticate(request, username = username, password = password)
-
-        # if user is not None:
-        #         login(request, user)
-        #         return redirect(request.GET['next'] if 'next' in request.GET else 'home')
-        # else:
-        #     messages.error(request, 'Username OR password is incorrect')
-
+            return redirect('login')
     else:
         return render(request, 'user/login.html')
+    
+def logoutView(request):
+    logout(request)
+    return redirect('home')
 
 def registerView(request):
     context = {}
