776a02b808368a29dd40a9e38ef2e1bb8f803c58
ShahZebYousafzai <zebshah7851@gmail.com>
2023-11-17 17:46:15 +0500

    Fixed the Responsiveness of Profile Info

--- GamersEdgeStore/settings.py
+++ GamersEdgeStore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!$enk%xbh6zh^nw+p5q%%15#tp3c*lac49b2ouhop2_h#o%gu9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.10']
 
 
 # Application definition
